211a9ae5f352dfb2a025884bb10b67f479d1c580
root <h.ahmed1@alustudent.com>
2022-07-08 04:00:45 -0700

    added 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     else:
         print('{} arguments:'.format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
-             if i > 0:
+            if i > 0:
                 print('{}: {}'.format(i, sys.argv[i]))

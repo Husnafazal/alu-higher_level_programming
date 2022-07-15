376e63a448d49750fa45d15c052b7b998a3cf5fa
root <h.ahmed1@alustudent.com>
2022-07-15 04:58:42 -0700

    added 11-delete-at.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) > 0:
-        return max(my_list)
         my_list.sort()
-        return my_list[-1]
+        print(max(my_list))
     else:
         return None
+        
+max_integer([1, 90, 2, 13, 34, 5, -13, 3])

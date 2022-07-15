72113569f140bc78503d31d4907ff5f9ff2f6628
root <h.ahmed1@alustudent.com>
2022-07-15 05:08:17 -0700

    added 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,6 @@
 def max_integer(my_list=[]):
     if len(my_list) > 0:
         my_list.sort()
-        print(max(my_list))
+        return my_list[-1]
     else:
         return None
-        
-max_integer([1, 90, 2, 13, 34, 5, -13, 3])
